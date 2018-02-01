f5a96e8e09f1488c56110d4dee38824d9943f10b
PieterjanRobbe <pieterjan.robbe@cs.kuleuven.be>
2018-02-01 17:17:34 +0100

    last checkpoint

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,6 @@
-## runtests.jl : run all test files
+# runtests.jl : run all MultilevelEstimators tests
 
 using MultilevelEstimators
 using Base.Test
 
-# toggle printing mode
-verbose = true
-
-# test indexsets
-#include("test_index_sets.jl")
-#include("test_number_generators.jl")
-include("test_covariance_functions.jl")
-#include("test_random_field_samplers.jl")
-#include("test_multilevelalgorithm.jl")
+include("test_index.jl")
--- test/test_karhunen_loeve_expansion.jl
+++
@@ -1,9 +0,0 @@
-## test_random_field_samplers : test for random field samplers 
-
-## EmptyGaussianRandomFieldSampler ## 
-verbose && print("testing empty gaussian random field sampler...")
-
-egrfs = EmptyGaussianRandomFieldSampler()
-@test egrfs === EmptyGaussianRandomFieldSampler()
-
-verbose && println("done")
